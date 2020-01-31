6d0ff62a75db9ce4981235bbcfa5be9f70d15fc6
Cody Persinger <codypersinger@gmail.com>
2020-01-30 21:32:27 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+import 'Styles/styles.scss';
+
+const App = () => {
+	return (
+		<div>
+            Hello World!
+		</div>
+	);
+};
+
+export default hot(App);
